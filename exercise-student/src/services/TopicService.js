bee8f044308c645a133383fe794388a820a613a0
lexxycool <maxirexy16@yahoo.com>
2021-10-06 12:09:57 -0400

    Add files via upload

---
+++ exercise-student/src/services/TopicService.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const http = axios.create({
+  baseURL: "http://localhost:3000"
+});
+
+export default {
+
+  list() {
+    return http.get('/topics');
+  },
+
+  get(id) {
+    return http.get(`/topics/${id}`);
+  },
+  
+  add(topic) {
+      return http.post('/topics', topic);
+  },
+
+  addTopic(id, topic) {
+    return http.put(`/topics/${id}`, topic);
+  }
+
+}
